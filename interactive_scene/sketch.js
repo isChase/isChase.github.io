9a0bd6d7eddf0d185e4408416d63aa0af4c21a4d
isChase <90922508+isChase@users.noreply.github.com>
2021-09-21 11:00:53 -0600

    wasd keys for interactive scene

--- interactive_scene/sketch.js
+++ interactive_scene/sketch.js
@@ -1,18 +1,49 @@
-// Project Title
-// Your Name
-// Date
+// Interactive scene
+// Chase
+// 9/21/2021
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-let theWidthOfSomeRectangle = 30;
+let x = 0;
+let y = 0;
+let radius = 25;
+let speed = 5;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+
+
 function draw() {
   background(220);
-  rect(mouseX, mouseY, theWidthOfSomeRectangle, 50);
-  ellipse(100, 200, 10, 10);
+  handleKeys();
+  noStroke();
+  rect(x, y, radius * 2);
+}
+function handleKeys(){
+  if (keyIsDown(38)){ // "w"
+    y -= speed;
+  }
+  if (keyIsDown(40)){ // "s"
+    y += speed;
+  }
+  if (keyIsDown(37)){ // "a"
+    x -= speed;
+  }
+  if (keyIsDown(39)){ //"d"
+    x += speed;
+  }
+  if (keyIsDown(87)){ // "up arrow"
+    y -= speed;
+  }
+  if (keyIsDown(83)){ // "down arrow"
+    y += speed;
+  }
+  if (keyIsDown(65)){ // "left arrow"
+    x -= speed;
+  }
+  if (keyIsDown(68)){ // "right arrow"
+    x += speed;
+  }
 }
