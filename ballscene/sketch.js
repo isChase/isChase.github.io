8be927b38bf3af3c6f5c2f07566131d167e3ba05
isChase <90922508+isChase@users.noreply.github.com>
2021-09-21 09:40:41 -0600

    added perlin noise

--- ballscene/sketch.js
+++ ballscene/sketch.js
@@ -33,9 +33,9 @@ function spawnBall(){
     ballColor: color(random(255), random(255), random(255), random(255)),
     dx: random(5, 10),
     dy: random(5, 10),
-    xTime: 0,
-    yTime: 100,
-    timeChange: random(0.001, 0.1),
+    xTime: random(1000),
+    yTime: random(1000),
+    timeChange: random(0.001, 0.01),
   };
   ballArray.push(newBall);
 }
@@ -45,8 +45,8 @@ function moveBall() {
     theBall.x = noise(theBall.xTime) * width;
     theBall.y = noise(theBall.yTime) * height;
     
-    theBall.xTime += 0.01;
-    theBall.yTime += 0.01;
+    theBall.xTime += theBall.timeChange;
+    theBall.yTime += theBall.timeChange;
   }
 }
 
