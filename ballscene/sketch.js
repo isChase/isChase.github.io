a27628d9ead5cb7f425848e462380375ae57da1e
isChase <90922508+isChase@users.noreply.github.com>
2021-09-21 09:33:31 -0600

    ballsceneUpdate

--- ballscene/sketch.js
+++ ballscene/sketch.js
@@ -32,17 +32,21 @@ function spawnBall(){
     radius: random(10, 30),
     ballColor: color(random(255), random(255), random(255), random(255)),
     dx: random(5, 10),
-    dy: random(5, 10)
+    dy: random(5, 10),
+    xTime: 0,
+    yTime: 100,
+    timeChange: random(0.001, 0.1),
   };
   ballArray.push(newBall);
 }
 
 function moveBall() {
   for (let theBall of ballArray) {
-    theBall.x += theBall.dx;
-    theBall.y += theBall.dy;
-    theBall.dx = random(-5, 5);
-    theBall.dy = random(-5, 5);   
+    theBall.x = noise(theBall.xTime) * width;
+    theBall.y = noise(theBall.yTime) * height;
+    
+    theBall.xTime += 0.01;
+    theBall.yTime += 0.01;
   }
 }
 
