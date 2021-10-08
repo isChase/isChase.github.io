98605e1e9a7d2c367afee71f316fe66d7c3b026b
isChase <90922508+isChase@users.noreply.github.com>
2021-10-08 10:50:05 -0600

    fireworks demo

---
+++ fireworks/sketch.js
@@ -0,0 +1,64 @@
+// Fireworks Demo
+
+let fireworks = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("black");
+
+  for (let i = fireworks.length-1; i >= 0; i--) {
+    if (fireworks[i].isDead()) {
+      //remove from array
+      fireworks.splice(i, 1);
+    }
+    else {
+      fireworks[i].update();
+      fireworks[i].display();
+    }
+  }
+}
+
+function mousePressed() {
+  angleMode(DEGREES);
+  let numberOfFireworks = 100;
+  for (let i = 0; i < numberOfFireworks; i++) {
+    let scalar = random(1, 3);
+    let dx = cos(i*360/numberOfFireworks + random(-3, 3)) * scalar;
+    let dy = sin(i*360/numberOfFireworks + random(-3, 3)) * scalar;
+    let someParticle = new Particle(mouseX, mouseY, dx, dy);
+    fireworks.push(someParticle);
+  }
+}
+
+
+class Particle {
+  constructor(x, y, dx, dy){
+    this.x = x;
+    this.y = y;
+    this.size = 10;
+    this.dx = dx;
+    this.dy = dy;
+    this.alpha = 255;
+    this.color = color(255,0 ,0, this.alpha);
+  }
+
+  display(){
+    noStroke();
+    this.color = color(255,0 ,0, this.alpha);
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+  }
+
+  update(){
+    this.x += this.dx;
+    this.y += this.dy;
+    this.alpha--;
+  }
+
+  isDead() {
+    return this.alpha <= 0;
+  }
+}
